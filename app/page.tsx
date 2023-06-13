36f386c7b4fb074215825cadd0394a9a5b2fc003
j3rryl <jeremymunroe0@gmail.com>
2023-06-13 19:43:47 +0300

    app bar

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,10 @@
 import Image from 'next/image'
+import ResponsiveAppBar from './components/ResponsiveAppBar'
 
 export default function Home() {
   return (
     <>
+    <ResponsiveAppBar />
     <h4>Home</h4>
     </>
   )
--- next.config.js
+++ next.config.js
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    modularizeImports:{
+    '@mui/icons-material':{
+      transform: '@mui/icons-material/{{member}}'
+    }
+  },
+  images: {
+    domains: ['images.unsplash.com','mavunobios.co.ke'],
+},
+}
 
 module.exports = nextConfig
